ef50b9f3f480ab5bd174e91860a23e9df4abcd4f
Nikolay Zhuravlev <znick@znick.ru>
2024-10-15 19:31:03 +0200

    + A=utf-8

--- db-sync.sh
+++ db-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/pt-table-sync --execute "u=$SRC_USER,p=$SRC_PASS,h=$SRC_HOST,D=$SRC_DB,t=$SRC_TABLE" "u=$DST_USER,p=$DST_PASS,h=$DST_HOST,D=$DST_DB,t=$DST_TABLE" --charset utf8 --function MD5
+/usr/bin/pt-table-sync --execute "A=utf-8,u=$SRC_USER,p=$SRC_PASS,h=$SRC_HOST,D=$SRC_DB,t=$SRC_TABLE" "A=utf-8,u=$DST_USER,p=$DST_PASS,h=$DST_HOST,D=$DST_DB,t=$DST_TABLE" --charset utf8 --function MD5
